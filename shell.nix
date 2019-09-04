502de7f785ee3745bc7f7a9ef8fe6dd8063ec8d8
samrose <samuel.rose@gmail.com>
2019-09-04 09:56:32 -0400

    shell qemu vm

--- shell.nix
+++ shell.nix
@@ -6,10 +6,23 @@ let
   root = toString ./.;
 in
 
-mkShell {
+stdenvNoCC.mkDerivation {
+  name = "example-nixpkgs";
+
+  shellHook = ''
+    example-switch() {
+      sudo -E nixos-rebuild switch --fast -I nixos-config=/etc/nixos/configuration.nix
+    }
+    example-shell() {
+      $(nix-build default.nix -A example-nixpkgs.qemu --no-out-link)/bin/run-nixos-vm
+    }
+  '';
+
   NIX_PATH = builtins.concatStringsSep ":" [
     "example-nixpkgs=${root}"
-    "nixpkgs=${root}/nixpkgs"
+    "nixpkgs=${pkgs.path}"
     "nixpkgs-overlays=${root}/overlays"
   ];
+
+  QEMU_OPTS = "-m 2048 -nographic";
 }
